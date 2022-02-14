a4839451fd905eab150d2be9f8a1ffab72e55dba
장성민 <jsm9358@naver.com>
2022-02-14 18:55:33 +0900

    Update python 딥러닝.py

--- python 딥러닝.py
+++ python 딥러닝.py
@@ -509,6 +509,7 @@ mnist_test = dset.MNIST("./", train=False, transform=transforms.ToTensor(), targ
 
 
 
+
     # 기본 CNN Ex - 1
 class CNN(nn.Module):
     def __init__(self):
@@ -539,7 +540,95 @@ class CNN(nn.Module):
         out = self.fc_layer(out)
         return out
 
+    
+    
+    
+    # VGG Ex - 1
+class VGG(nn.Modules):
+    def __init__(self, features, num_classes=1000, init_weights=True):
+       super(VGG, self).__init__()
+       self.features = features
+       self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+       self.classifier = nn.Sequential(
+           nn.Linear(512 * 7 * 7, 4096),
+           nn.ReLU(True),
+           nn.Linear(4096, 4096),
+           nn.ReLU(True),
+           nn.Linear(4096, num_classes),
+       )
+       if init_weights:
+           self.initialize_weights()
+            
+   def forward(self, x):
+       x = self.features(x)
+       x = self.avgpool(x)
+       x = x.view(x.size(0), -1)
+       x = self.classifier(x)
+       return x
+    
+   def _initialize_weights(self):
+       for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weights, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, 0, 0.01)
+                    nn.init.constant_(m.bias, 0)
+                    
+   def make_layers(cfg, batch_norm=False):
+      layers = []
+      in_channels = 3
+      for v in cfg:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                con2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [con2d, nn.ReLU(inplace=True)]
+                in_channels = v
+    return nn.Sequential(*layers)
+
+
+
+    cfgs = {
+       'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+       'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+       'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+       'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    }
+        
+                 
+                 
+                 
+                 
+                 
+                    
+                    
+                    
+                    
+                 
+                 
+                 
+                 
+                 
 ---------- RNN ----------
+
+
+
+
+
+
+
+
+
+
+
 
 
 
